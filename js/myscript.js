f30c46b9ec07bf62a4d9e738041b690b5c5643c7
rhdms1035 <kkokkoning@naver.com>
2020-06-09 16:06:49 +0900

    6월9일로고카톡추가

--- js/myscript.js
+++ js/myscript.js
@@ -236,7 +236,7 @@ $("#header .depth1 > li").on("click", function(){
 // 2500픽셀로 가면 컨택트 텍스트애니메이션효과 나타나게 하기
 $(window).scroll(function(){
     var sct = $(this).scrollTop();
-        if (sct >2500) {
+        if (sct >3000) {
             $(".contact").addClass("on")
         } else {
             $(".contact").removeClass("on")
